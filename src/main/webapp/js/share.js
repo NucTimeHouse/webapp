5c08a3aeae5b93ba175492b2a88577899c1a18d4
qiushibo <1114676346@qq.com>
2022-04-14 14:03:29 +0800

    解析url

--- src/main/webapp/js/share.js
+++ src/main/webapp/js/share.js
@@ -1,15 +1,39 @@
-function getSharePhotos(e){
-    var deleteid=e.dataset.deleted;
-    var aid = e.dataset.aid;
-    var userId=e.dataset.userId;
+var url=window.location
+
+
+const getQuery = (url) => {
+    // str为？之后的参数部分字符串
+    const str = url.substr(url.indexOf('?') + 1)
+    // arr每个元素都是完整的参数键值
+    const arr = str.split('&')
+    // result为存储参数键值的集合
+    const result = {}
+    for (let i = 0; i < arr.length; i++) {
+        // item的两个元素分别为参数名和参数值
+        const item = arr[i].split('=')
+        result[item[0]] = item[1]
+    }
+    return result[query]
+}
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+}
+function getSharePhotos(){
+    // const res = getQuery(url)
+
     $.ajax({
         url:"photo/share/photos",
         type:"GET",
         dataType:"JSON",
         data:{
-            deleted: deleteid,
-            aid:aid,
-            userId:userId
+            deleted: getQueryString("deleted"),
+            aid: getQueryString("aid"),
+            userId: getQueryString("userId"),
         },
         success:(res)=>{
             sphoto.sPhotosData = res
